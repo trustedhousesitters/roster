7419a227ce6e6e6b51e36874d4dd7cd092506485
Will Ogden <will.ogden@gmail.com>
2016-01-13 11:38:05 +0000

    Return active bool

--- client.go
+++ client.go
@@ -162,7 +162,7 @@ func (c *Client) Register(name string, endpoint string) (*Service,error) {
 func (c *Client) Discover(name string) (*Service, error) {
 
 	// Make sure registry is active
-	if c.Registry.IsActive() {
+	if active,err := c.Registry.IsActive(); active == true {
 		expressionAttributeValues := map[string]interface{}{
 	    	":NameVal": name,
 	    	":ExpiryVal":   time.Now().Unix(),
